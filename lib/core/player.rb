8d399ab314f2b27c0d9a9cb05e11add33782a9b4
Hyftar <simon_landry@ymail.com>
2016-07-31 06:08:27 -0400

    Add score-related methods to player class

--- lib/core/player.rb
+++ lib/core/player.rb
@@ -1,8 +1,22 @@
+require './tetrimino'
+
 class Player
     def initialize(game)
         @score = 0
         @linesCleared = 0
+        @combo = 0
         @game = game
         @board = Board.new
+    end
+
+    # Increments the score of the player relative to the number of lines he just cleared and his current combo
+    def increment_score(lines)
+        @combo += 1
+        @linesCleared += lines
+        @score += 100 * 2 ** (lines - 1) * combo
+    end
+
+    def reset_combo
+        @combo = 0
     end
 end
