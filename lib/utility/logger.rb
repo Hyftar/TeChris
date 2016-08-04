4c648bb5994d46b469c13c34184e92de84030b23
Hyftar <simon_landry@ymail.com>
2016-08-04 06:27:49 -0400

    Add logger class

---
+++ lib/utility/logger.rb
@@ -0,0 +1,9 @@
+class Logger
+    def initialize
+        @file = File.new("logs/#{Time.now.strftime('%Y-%m-%d')}.log", 'a')
+    end
+
+    def log(data)
+        @file.write("[#{Time.now.strftime('%H:%M:%S')}] #{data}\n")
+    end
+end
