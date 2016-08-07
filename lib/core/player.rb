57294eb884c4d1a9a9b83e75b110c4588c2f1f80
Hyftar <simon_landry@ymail.com>
2016-08-07 07:44:26 -0400

    Clean player class

--- lib/core/player.rb
+++ lib/core/player.rb
@@ -1,19 +1,33 @@
 require './core/tetrimino'
 
 class Player
+
+  attr_reader :score, :board, :combo, :lines_cleared
+
   def initialize(game)
     @score = 0
-    @linesCleared = 0
+    @lines_cleared = 0
     @combo = 0
     @game = game
-    @board = Board.new
+
+    # TODO: Move the tetriminos definitions from here.
+
+    i = Tetrimino.new([[1, 1, 1, 1]], Color::CYAN)
+    o = Tetrimino.new([[1, 1], [1, 1]], Color::YELLOW)
+    t = Tetrimino.new([[0, 1, 0], [1, 1, 1]], Color::PURPLE)
+    s = Tetrimino.new([[0, 1, 1], [1, 1, 0]], Color::LIME)
+    z = Tetrimino.new([[1, 1, 0], [0, 1, 1]], Color::RED)
+    j = Tetrimino.new([[1, 0, 0], [1, 1, 1]], Color::BLUE)
+    l = Tetrimino.new([[0, 0, 1], [1, 1, 1]], Color::ORANGE)
+
+    @board = Board.new(self, 10, 22, 5, i, o, t, s, z, j, l)
   end
 
   # Increments the score of the player relative to the number of lines he just cleared and his current combo
   def increment_score(lines)
     unless lines == 0
       @combo += 1
-      @linesCleared += lines
+      @lines_cleared += lines
       @score += 100 * 2**(lines - 1) * combo
     end
   end
