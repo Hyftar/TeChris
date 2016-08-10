e3b559a96fccf374b9892a2de8ccf87b25c10931
Hyftar <simon_landry@ymail.com>
2016-08-10 05:31:21 -0400

    Convert logger to Singleton class

--- lib/utility/logger.rb
+++ lib/utility/logger.rb
@@ -1,6 +1,7 @@
 require 'digest/md5'
+require 'singleton'
 class Logger
-
+  include Singleton
   def initialize
     @file = File.new("../logs/#{Time.now.strftime('%Y-%m-%d')} - #{Digest::MD5.hexdigest rand.to_s}.log", 'a')
   end
